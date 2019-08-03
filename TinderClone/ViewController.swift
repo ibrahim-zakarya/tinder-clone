a23027b3cdcb3f83fcd29715631a5027f52db2c9
Ibrahim Zakarya <ibrahim.zakarya93@gmail.com>
2019-08-03 18:42:52 +0300

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Ibrahim  Zakarya  on 8/3/19.
+//  Copyright © 2019 Ibrahim  Zakarya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
